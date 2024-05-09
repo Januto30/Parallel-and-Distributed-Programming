b546e21fc7cafe1bdf7b1a2f0785063640c82df6
Januto30 <jan.prats04@gmail.com>
2024-05-09 08:53:37 +0200

    as

--- P2/1_communications/mpi_comms amb apunts.c
+++ P2/1_communications/mpi_comms amb apunts.c
@@ -24,14 +24,18 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    /// FIRST TEST - SYNCHRONOUS COMMUNICATION
     test_synchronous(rank, size);
 
-    test_asynchronous(rank, size);
+    /// FIRST TEST - ASYNCHRONOUS COMMUNICATION
+    //test_asynchronous(rank, size);
 
     MPI_Finalize();
     return 0;
 }
 
+
+//Utilitzar funcions blocking
 void test_synchronous(int rank, int size)
 {
     double start_time, run_time, average_time, max_time, min_time;
@@ -39,31 +43,56 @@ void test_synchronous(int rank, int size)
     int rbuf[NUM_TEST];
     MPI_Status status;
 
+    /// TODO compute send/receive cores
     int send_core = (rank + 1) % size;
     int recv_core = (rank - 1 + size) % size;
+    /// TODO
 
     start_time = MPI_Wtime();
     for(int i = 0; i < NUM_TEST; i++) {
         sbuf[i] = rank;
 
+        /// TODO send
+        /**
+         * Envia un enter individual de l'array sbuf a un core específic utilitzant MPI_Send.
+         * @param sbuf L'array que conté els enters a enviar.
+         * @param i L'índex de l'enter a enviar.
+         * @param send_core El rang del core al qual s'enviarà l'enter.
+         * @param tag L'etiqueta del missatge.
+         * @param comm El comunicador utilitzat per a la comunicació.
+         */
         MPI_Send(&sbuf[i], 1, MPI_INT, send_core, 0, MPI_COMM_WORLD);
+        /// TODO
 
         if (i % size == rank)
             usleep(WAITING_TIME);
 
+        /// TODO recv
+        /**
+         * Rep un enter individual d'un core específic utilitzant MPI_Recv.
+         * @param rbuf L'array on es guardarà l'enter rebut.
+         * @param i L'índex de l'enter a rebre.
+         * @param recv_core El rang del core del qual es rebra l'enter.
+         * @param tag L'etiqueta del missatge.
+         * @param comm El comunicador utilitzat per a la comunicació.
+         * @param status L'estat de la recepció.
+         */
         MPI_Recv(&rbuf[i], 1, MPI_INT, recv_core, 0, MPI_COMM_WORLD, &status);
+        /// TODO
 
         assert(rbuf[i] == recv_core);
     }
     run_time = MPI_Wtime() - start_time;
 
-    MPI_Reduce(&run_time, &average_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
-    MPI_Reduce(&run_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&run_time, &min_time, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
-    average_time /= size;
+    /// TODO compute average, max and min times
+    double avarage_time, max_time, min_time;
+    MPI_Reduce(&run_time, &avarage_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); //MPI_SUM suma tots els temps de tots els processos
+    MPI_Reduce(&run_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD); //MPI_MAX agafa el temps màxim de tots els processos
+    MPI_Reduce(&run_time, &min_time, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD); //MPI_MIN agafa el temps mínim de tots els processos
+    avarage_time /= size;
 
     if (rank == 0) {
-        printf("Synchronous send/receive test with %d processes and %d repetitions.\n", size, NUM_TEST);
+        printf("Asynchronous send/receive test with %d processes and %d repetitions.\n", size, NUM_TEST);
         printf("    average: %.2lf s\n", average_time);
         printf("    min:     %.2lf s\n", min_time);
         printf("    max:     %.2lf s\n", max_time);
@@ -72,7 +101,6 @@ void test_synchronous(int rank, int size)
     MPI_Barrier(MPI_COMM_WORLD);
 
 }
-
 
 void test_asynchronous(int rank, int size)
 {
@@ -85,20 +113,20 @@ void test_asynchronous(int rank, int size)
 
     start_time = MPI_Wtime();
 
-    MPI_Request request[NUM_TEST*2];
     for(int i = 0; i < NUM_TEST; i++) {
         sbuf[i] = rank;
 
-        MPI_Isend(&sbuf[i], 1, MPI_INT, send_core, 0, MPI_COMM_WORLD, &request[2*i]);
+        MPI_Request request;
+        MPI_Isend(&sbuf[i], 1, MPI_INT, send_core, 0, MPI_COMM_WORLD, &request);
 
         if (rank == i)
             usleep(WAITING_TIME);
 
-        MPI_Irecv(&rbuf[i], 1, MPI_INT, recv_core, 0, MPI_COMM_WORLD, &request[2*i+1]);
+        MPI_Irecv(&rbuf[i], 1, MPI_INT, recv_core, 0, MPI_COMM_WORLD, &request);
     }
 
-    MPI_Status status[NUM_TEST*2];
-    MPI_Waitall(NUM_TEST*2, request, status);
+    MPI_Status status;
+    MPI_Waitall(NUM_TEST, request, &status);
 
     run_time = MPI_Wtime() - start_time;
 
